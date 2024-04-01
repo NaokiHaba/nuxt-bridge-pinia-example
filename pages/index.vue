d78cb4fb154ad2ebe70fcdf9acdef964c55482d5
Naoki Haba <naosprintrunner6385@gmail.com>
2024-04-02 04:14:04 +0900

    Add counter store with Pinia and update index.vue

--- pages/index.vue
+++ pages/index.vue
@@ -1,9 +1,27 @@
 <template>
-  <Tutorial/>
+  <div>
+    <h1>Todo</h1>
+    <p>Count: {{ counter.getCounter }}</p>
+    <button @click="counter.increment">Increment</button>
+    <div v-if="user">
+      <p>Name: {{ user.name }}</p>
+      <p>Email: {{ user.email }}</p>
+    </div>
+  </div>
 </template>
 
 <script>
+import { useCounterStore } from "~/stores/counter"
+
 export default {
-  name: 'IndexPage'
-}
+  setup() {
+    const counter = useCounterStore();
+    return { counter };
+  },
+  async asyncData({ $pinia }) {
+    const counter = useCounterStore($pinia);
+    await counter.fetch();
+    return { user: counter.getUser };
+  }
+};
 </script>

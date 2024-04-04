19b42a80941ed6ea68966e1bbc0dd851b05ed32d
Naoki Haba <naosprintrunner6385@gmail.com>
2024-04-05 03:58:58 +0900

    Move User details and Counter functionalities to About page

---
+++ pages/about.vue
@@ -0,0 +1,27 @@
+<script>
+import { useUserStore } from "~/stores/UserStore"
+import { mapState } from "pinia"
+
+export default {
+  async fetch({ $pinia }) {
+    const store = useUserStore($pinia)
+    await store.fetchUserByID()
+  },
+  computed: {
+    ...mapState(useUserStore, {
+      userInfo: (state) => {
+        return state.userInfo
+      }
+    })
+  }
+};
+</script>
+
+<template>
+  <div>
+    <h1>ユーザー詳細</h1>
+    {{ userInfo}}
+  </div>
+
+</template>
+
--- pages/index.vue
+++ pages/index.vue
@@ -1,27 +1,5 @@
 <template>
   <div>
-    <h1>Todo</h1>
-    <p>Count: {{ counter.getCounter }}</p>
-    <button @click="counter.increment">Increment</button>
-    <div v-if="user">
-      <p>Name: {{ user.name }}</p>
-      <p>Email: {{ user.email }}</p>
-    </div>
+    <router-link to="/about">ユーザー詳細</router-link>
   </div>
 </template>
-
-<script>
-import { useCounterStore } from "~/stores/counter"
-
-export default {
-  setup() {
-    const counter = useCounterStore();
-    return { counter };
-  },
-  async asyncData({ $pinia }) {
-    const counter = useCounterStore($pinia);
-    await counter.fetch();
-    return { user: counter.getUser };
-  }
-};
-</script>

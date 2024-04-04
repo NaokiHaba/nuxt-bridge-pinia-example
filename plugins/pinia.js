cfffcf29f14ee429660f9096f6a0751e0555c430
Naoki Haba <naosprintrunner6385@gmail.com>
2024-04-05 03:59:10 +0900

    Expanded Pinia plugin functionality and configuration

--- plugins/pinia.js
+++ plugins/pinia.js
@@ -1,8 +1,30 @@
-import { createPinia } from 'pinia';
-import Vue from 'vue';
+import { createPinia, PiniaVuePlugin, setActivePinia } from 'pinia'
+import Vue from 'vue'
 
-export default ({ app }, inject) => {
-  const pinia = createPinia();
-  Vue.use(pinia);
-  inject('pinia', pinia);
-};
+Vue.use(PiniaVuePlugin)
+
+export default (context, provide) => {
+  const pinia = createPinia()
+  context.app.pinia = pinia
+
+  // 複数のStoreがそれぞれのStoreに適切に紐づくようにする
+  setActivePinia(pinia)
+
+  // HACK: デフォルトはPiniaは自身のStoreインスタンスのみを参照するため、Piniaの内部プラグインに対してNuxtのコンテキストを渡す
+  // https://github.com/vuejs/pinia/blob/v2/packages/pinia/src/store.ts#L698
+  pinia._p.push(({ store }) => {
+    Object.defineProperty(store, '$nuxt', { value: context })
+  })
+
+  // if (process.server) {
+  //   // nuxtServerInitの後にSSRの結果をフロントと同期（Nuxt3では不要）
+  //   // レンダリング前にPiniaの状態をnuxtStateオブジェクトに保存しないと自動では同期されない
+  //   context.beforeNuxtRender((ctx) => {
+  //     ctx.nuxtState.pinia = pinia.state.value
+  //   })
+  // } else if (context.nuxtState && context.nuxtState.pinia) {
+  //   pinia.state.value = context.nuxtState.pinia
+  // }
+
+  provide('pinia', pinia)
+}
